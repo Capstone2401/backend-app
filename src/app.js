b5c4a10dc527047547643e768befdb3d2fa98e77
Gianni Barber <giannibarber@protonmail.com>
2024-03-14 18:58:51 +0100

    Added initial Scaffolding for the App
    Co-authored-by: Brandon-Corey <bcorey3660@gmail.com>
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const config = require("./utils/config");
+const morgan = require("morgan");
 
 const app = express();
 const host = config.HOST || "localhost";
 const port = config.PORT || 3000;
 
+app.use(morgan("common"));
+
+app.get("/data", (_req, _res, next) => {
+  try {
+    // STUB
+  } catch (error) {
+    next(error);
+  }
+});
+
 // Error handler
 app.use((err, _req, res, _next) => {
-  console.log(err); // Writes more extensive information to the console log
-  res.status(404).send(err.message);
+  console.error(err); // Writes more extensive information to the console log
+  res.status(404).send(err.message); // Tranform to more granular error messages for 4XX and 5XX
 });
 
 // Listener
