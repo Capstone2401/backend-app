657e8b8089013114e8f78a897f47096c8e0a339c
Brandon Corey <bcorey3660@gmail.com>
2024-03-20 19:41:46 -0400

    add test routes for docker

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 "use strict";
 
+require("dotenv").config();
 const express = require("express");
 const config = require("./utils/config");
 const morgan = require("morgan");
+const redshift = require("./lib/redshift-pg");
 
 const app = express();
+
 const host = config.HOST || "localhost";
 const port = config.PORT || 3000;
 
 app.use(morgan("common"));
+
+app.get("/", (_req, res) => res.send("hello world"));
+
+app.get("/test", async (_req, res) => {
+  const result = await redshift.events.listAll();
+  res.status(200).send(JSON.stringify(result));
+});
 
 app.get("/data", (_req, _res, next) => {
   try {
@@ -25,6 +35,6 @@ app.use((err, _req, res, _next) => {
 });
 
 // Listener
-app.listen(port, host, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`App is listening on port ${port} of ${host}.`);
 });
