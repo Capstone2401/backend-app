5071804dd5d231959470763c6baed7458e26e352
Brandon Corey <bcorey3660@gmail.com>
2024-03-28 12:46:16 -0400

    modularize middleware functions for application
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

---
+++ src/middleware/attributes.js
@@ -0,0 +1,10 @@
+async function handleQueryAttributes(_req, res, next) {
+  try {
+    const result = await redshift.getAllAttributes();
+    res.status(200).send(JSON.stringify(result));
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { handleQueryAttributes };
