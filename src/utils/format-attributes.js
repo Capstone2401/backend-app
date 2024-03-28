f6fd0c7fc8644c41afb7235fc3657708b5defb3d
Brandon Corey <bcorey3660@gmail.com>
2024-03-27 20:52:26 -0400

    fix incorrect variable name causing an 'undefined' method call error

--- src/utils/format-attributes.js
+++ src/utils/format-attributes.js
@@ -1,27 +1,28 @@
 function formatAttributes(result) {
   let attributeObj = {
     event: getDistinctAttributes(result[0]),
-    user: getDistinctAttributes(result[1])
-  }
+    user: getDistinctAttributes(result[1]),
+  };
 
   return attributeObj;
 }
 
 function getDistinctAttributes(attributeArr) {
-  let formattedAttributes = {};
-  attributeArr.rows.forEach(row => {
+  let formattedAtrributes = {};
+  attributeArr.rows.forEach((row) => {
     let attributes = JSON.parse(row.json_serialize);
     for (let attribute in attributes) {
       let value = attributes[attribute];
-      if (!newObj.hasOwnProperty(attribute)) {
-        newObj[attribute] = [];
+      if (!formattedAtrributes.hasOwnProperty(attribute)) {
+        formattedAtrributes[attribute] = [];
       }
-      if (!newObj[attribute].includes(value)) {
-        newObj[attribute].push(String(value))
+      if (!formattedAtrributes[attribute].includes(value)) {
+        formattedAtrributes[attribute].push(String(value));
       }
     }
-  })
-  return formattedAttributes;
+  });
+
+  return formattedAtrributes;
 }
 
-module.exports = formatAttributes
+module.exports = formatAttributes;
