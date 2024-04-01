771d9cb7a4b0000600565e4bcd63ee7477c4bf1a
Brandon Corey <bcorey3660@gmail.com>
2024-04-01 02:44:17 -0400

    refactor routers to be implemented in typescript
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

---
+++ src/routes/info.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+import { handleQueryAttributes } from "../middleware/attributes";
+import { handleQueryEventNames } from "../middleware/events";
+
+router.get("/eventNames", handleQueryEventNames);
+router.get("/attributes", handleQueryAttributes);
+
+export default router;
---
+++ src/routes/query.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+import { handleQueryData } from "../middleware/data";
+
+router.post("/events", handleQueryData);
+router.post("/users", handleQueryData);
+
+export default router;
