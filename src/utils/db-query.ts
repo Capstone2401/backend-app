579fbe61d2cca7cd0b44b11a1b465453c9075a60
Brandon Corey <bcorey3660@gmail.com>
2024-04-01 02:40:37 -0400

    refactor db-query utility function to use typescript
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/utils/db-query.ts
+++ src/utils/db-query.ts
@@ -1,15 +1,17 @@
-"use strict";
+import { Client } from "pg";
+import { QueryResult } from "pg";
+import config from "./config";
 
-const { Client } = require("pg");
-const config = require("./config");
-
-const logQuery = (statement, parameters) => {
+function logQuery(statement: string, parameters: string[]): void {
   let timeStamp = new Date();
   let formattedTimeStamp = timeStamp.toString().substring(4, 24);
   console.log(formattedTimeStamp, statement, parameters);
-};
+}
 
-const dbQuery = async (statement, ...parameters) => {
+async function dbQuery(
+  statement: string,
+  ...parameters: string[]
+): Promise<QueryResult | Error> {
   try {
     let client = new Client({ connectionString: config.REDSHIFT_CONN_STRING });
     await client.connect();
@@ -21,8 +23,12 @@ const dbQuery = async (statement, ...parameters) => {
 
     return result;
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) {
+      return error;
+    }
   }
-};
+
+  return new Error("Unknown error occured");
+}
 
 module.exports = dbQuery;
