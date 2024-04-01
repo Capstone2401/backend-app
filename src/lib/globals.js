e7d72fd818b876d7f78c523ab0dd884857629d6f
Brandon Corey <bcorey3660@gmail.com>
2024-04-01 02:41:42 -0400

    refactor globals to use typescript with custom interface and type literal types
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/lib/globals.js
+++
@@ -1,5 +0,0 @@
-const VALID_TIME_UNIT = { hour: "hour", day: "day", month: "month" };
-
-module.exports = {
-  VALID_TIME_UNIT,
-};
---
+++ src/lib/globals.ts
@@ -0,0 +1,9 @@
+import { TimeUnitValidation } from "../../types/valid-time";
+
+const VALID_TIME_UNIT: TimeUnitValidation = {
+  hour: true,
+  day: true,
+  month: true,
+};
+
+export { VALID_TIME_UNIT };
