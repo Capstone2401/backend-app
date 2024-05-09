c2a8c9ba1398faa2e69de8222746d669aee1c2da
Brandon Corey <bcorey3660@gmail.com>
2024-05-09 18:29:15 -0400

    update to median query which was incorrect before
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/sql/events.ts
+++ src/sql/events.ts
@@ -93,30 +93,30 @@ function getMinPerUserBy(timeUnit: TimeUnit): string {
 
   return `
     WITH events_per_user_per_${timeUnit} AS (
+      SELECT
+        DATE_TRUNC('${timeUnit}', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
+        COUNT(DISTINCT e.id) AS calculated_value,
+        e.user_id
+      FROM
+        events e
+      LEFT JOIN
+        users u ON u.user_id = e.user_id
+      WHERE
+        (CAST($1 AS TIMESTAMP) IS NULL OR event_created BETWEEN $1 AND SYSDATE)
+        AND (CAST($2 AS VARCHAR) IS NULL OR event_name = $2)
+        AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
+        AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
+      GROUP BY
+        ${timeUnit}, e.user_id
+    )
     SELECT
-      DATE_TRUNC('${timeUnit}', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
-      COUNT(DISTINCT e.id) AS calculated_value,
-      e.user_id
+      ${timeUnit},
+      MIN(calculated_value) AS calculated_value
     FROM
-      events e
-    LEFT JOIN
-      users u ON u.user_id = e.user_id
-    WHERE
-      (CAST($1 AS TIMESTAMP) IS NULL OR event_created BETWEEN $1 AND SYSDATE)
-      AND (CAST($2 AS VARCHAR) IS NULL OR event_name = $2)
-      AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
-      AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
+      events_per_user_per_${timeUnit}
     GROUP BY
-      ${timeUnit}, e.user_id
-  )
-  SELECT
-    ${timeUnit},
-    MIN(calculated_value) AS calculated_value
-  FROM
-    events_per_user_per_${timeUnit}
-  GROUP BY
-    ${timeUnit}
-  `;
+      ${timeUnit}
+    `;
 }
 
 function getMaxPerUserBy(timeUnit: TimeUnit): string {
@@ -129,29 +129,29 @@ function getMaxPerUserBy(timeUnit: TimeUnit): string {
 
   return `
     WITH events_per_user_per_${timeUnit} AS (
+      SELECT
+        DATE_TRUNC('${timeUnit}', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
+        COUNT(DISTINCT e.id) AS calculated_value,
+        e.user_id
+      FROM
+        events e
+      LEFT JOIN
+        users u ON u.user_id = e.user_id
+      WHERE
+        (CAST($1 AS TIMESTAMP) IS NULL OR event_created BETWEEN $1 AND SYSDATE)
+        AND (CAST($2 AS VARCHAR) IS NULL OR event_name = $2)
+        AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
+        AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
+      GROUP BY
+        ${timeUnit}, e.user_id
+    )
     SELECT
-      DATE_TRUNC('${timeUnit}', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
-      COUNT(DISTINCT e.id) AS calculated_value,
-      e.user_id
+      ${timeUnit},
+      MAX(calculated_value) AS calculated_value
     FROM
-      events e
-    LEFT JOIN
-      users u ON u.user_id = e.user_id
-    WHERE
-      (CAST($1 AS TIMESTAMP) IS NULL OR event_created BETWEEN $1 AND SYSDATE)
-      AND (CAST($2 AS VARCHAR) IS NULL OR event_name = $2)
-      AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
-      AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
+      events_per_user_per_${timeUnit}
     GROUP BY
-      ${timeUnit}, e.user_id
-  )
-  SELECT
-    ${timeUnit},
-    MAX(calculated_value) AS calculated_value
-  FROM
-    events_per_user_per_${timeUnit}
-  GROUP BY
-    ${timeUnit}
+      ${timeUnit}
   `;
 }
 
@@ -164,43 +164,37 @@ function getMedianPerUserBy(timeUnit: TimeUnit): string {
   }
 
   return `
-    WITH event_counts AS (
+    WITH events_per_user_per_${timeUnit} AS (
       SELECT
-        e.user_id,
-        DATE_TRUNC('hour', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
-        COUNT(DISTINCT e.id) as num_events
-      FROM events e
-      LEFT JOIN
-        users u ON u.user_id = e.user_id
-      WHERE
-        (CAST($1 AS TIMESTAMP) IS NULL OR e.event_created BETWEEN $1 AND SYSDATE)
-        AND (CAST($2 AS VARCHAR) IS NULL OR e.event_name = $2)
-        AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
-        AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
-      GROUP BY
-        e.user_id,
-        ${timeUnit}
-    ),
-    user_event_counts AS (
-      SELECT
-        u.user_id,
-        u.user_created,
-        COALESCE(ec.${timeUnit}, (SELECT ${timeUnit} FROM event_counts WHERE ${timeUnit} IS NOT NULL LIMIT 1)) AS ${timeUnit},
-        CAST(COALESCE(ec.num_events, 0) AS INT) AS num_events
+          e.user_id,
+          DATE_TRUNC('${timeUnit}', CAST(event_created AS TIMESTAMPTZ)) AS ${timeUnit},
+          COALESCE(COUNT(*), 0) AS count
       FROM
-        users u
-      LEFT JOIN event_counts ec ON
-        u.user_id = ec.user_id
+          events e
+      LEFT JOIN
+      users u ON e.event_created <= u.user_created
+      WHERE
+          (CAST($1 AS TIMESTAMP) IS NULL OR e.event_created BETWEEN $1 AND SYSDATE)
+          AND (CAST($2 AS VARCHAR) IS NULL OR e.event_name = $2)
+          AND (CAST($3 AS VARCHAR) IS NULL OR attr_validate($3, JSON_SERIALIZE(e.event_attributes)))
+          AND (CAST($4 AS VARCHAR) IS NULL OR attr_validate($4, JSON_SERIALIZE(u.user_attributes)))
+      GROUP BY
+          e.user_id, ${timeUnit}
+  ),
+  medians_per_${timeUnit} AS (
+      SELECT
+          ${timeUnit},
+          MEDIAN(count) AS median_count
+      FROM
+          events_per_user_per_${timeUnit}
+      GROUP BY
+          ${timeUnit}
     )
     SELECT
-      ${timeUnit},
-      CAST(MEDIAN(num_events) AS FLOAT) AS calculated_value
+        ${timeUnit},
+        median_count AS calculated_value
     FROM
-      user_event_counts
-    WHERE user_created <= ${timeUnit}
-    GROUP BY
-    ${timeUnit}
-  `;
+        medians_per_${timeUnit};  `;
 }
 
 export default {
